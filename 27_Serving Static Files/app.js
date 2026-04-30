c7fd5405da0af36037a52dada36332768acc3e5c
HansonJoel <joelhanson002@gmail.com>
2026-04-30 15:28:03 +0100

    moved 404 handler to active middleware for improved error handling

--- 27_Serving Static Files/app.js
+++ 27_Serving Static Files/app.js
@@ -33,12 +33,12 @@ app.get("/", (req, res) => {
   res.send("Welcome to my Express.js application!");
 });
 
-// 5. 404 handler
-// app.use((req, res) => {
-//   res.status(404).json({
-//     status: "fail",
-//     message: "Route not found",
-//   });
-// });
+//5. 404 handler
+app.use((req, res) => {
+  res.status(404).json({
+    status: "fail",
+    message: "Route not found",
+  });
+});
 
 module.exports = app;
