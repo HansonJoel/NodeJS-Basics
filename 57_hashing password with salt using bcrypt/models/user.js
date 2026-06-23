51d26fcd0e32ab15c01c6c13a509d8623ab1ac26
HansonJoel <joelhanson002@gmail.com>
2026-06-23 17:31:17 +0100

    i integrated a function to hash the password using bcrypt

---
+++ 57_hashing password with salt using bcrypt/models/user.js
@@ -0,0 +1,67 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+const bcrypt = require("bcryptjs");
+
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      trim: true,
+      lowercase: true,
+      required: [true, "First Name is required"],
+      validate: [validator.isAlpha, "First name can only contain letters"],
+    },
+    lastName: {
+      type: String,
+      trim: true,
+      lowercase: true,
+      required: true,
+      validate: [validator.isAlpha, "First name can only contain letters"],
+    },
+    email: {
+      type: String,
+      required: [true, "Please add an email address"],
+      trim: true,
+      lowercase: true,
+      unique: [true, "A user with same email already exist"],
+      validate: [validator.isEmail, "Email is not valid"],
+    },
+    photo: String,
+    password: {
+      type: String,
+      required: [true, "Kindly enter your password"],
+      minLength: 8,
+    },
+    confirmPassword: {
+      type: String,
+      required: [true, "Kindly confirm your password"],
+      validate: {
+        validator: function (value) {
+          return value === this.password;
+        },
+        message: "Passwords do not match",
+      },
+    },
+  },
+  { timestamps: true },
+);
+
+userSchema.pre("save", async function () {
+  // skip hashing if password is not modified
+  if (!this.isModified("password")) return next();
+  // hashing the password before saving it to the database
+
+  this.password = await bcrypt.hash(this.password, 10); // using the salt 10
+  this.confirmPassword = undefined;
+});
+
+// userSchema.pre("save", async function (next) {
+//   if (!this.isModified("password")) return next();
+
+//   this.password = await bcrypt.hash(this.password, 12);
+
+//   this.confirmPassword = undefined;
+
+//   next();
+// });
+module.exports = mongoose.model("user", userSchema);
